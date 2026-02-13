18278d9e11f85aad8c94fee094ba58634c9d34bc
Vishal Prajapati <52890952+Vishal01-git@users.noreply.github.com>
2026-02-13 16:48:36 +0530

    newer version index

--- templates/report.js
+++ templates/report.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Auto-expand tables with errors
     document.querySelectorAll('.table-card.error details').forEach(details => {
         details.setAttribute('open', '');
     });
